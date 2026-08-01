29aae1a7b7f2db1e05752c1887ef661edec8232f
Masha Khodina <marie.hodina@yandex.by>
2026-08-01 14:34:15 +0300

    ред3

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -28,8 +28,12 @@ export const BurgerConstructor: FC = () => {
     ingredients: currentIngredients
   };
 
-  const isOrderRequesting = useSelector((state) => state.burgerOrder.orderRequest);
-  const orderDetailsData = useSelector((state) => state.burgerOrder.orderModalData);
+  const isOrderRequesting = useSelector(
+    (state) => state.burgerOrder.orderRequest
+  );
+  const orderDetailsData = useSelector(
+    (state) => state.burgerOrder.orderModalData
+  );
   const currentUser = useSelector((state) => state.burgerUser.user);
 
   const handleOrderClick = () => {
@@ -74,7 +78,7 @@ export const BurgerConstructor: FC = () => {
   );
 
   return (
-    <div data-testid="burger-constructor">
+    <div data-testid='burger-constructor'>
       <BurgerConstructorUI
         price={totalPrice}
         orderRequest={isOrderRequesting}
